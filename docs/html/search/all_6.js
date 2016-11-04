d51f0d71e18e584824cd22de7b8d88053108d46d
Shindo <cn.shindo@gmail.com>
2016-11-04 15:48:54 -0700

    Update docs.

--- docs/html/search/all_6.js
+++ docs/html/search/all_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['indextomatrix',['indexToMatrix',['../namespace_n_puzzle.html#a6bc1d43df14bf169c99aa5ea797243c4',1,'NPuzzle']]],
+  ['indextomatrix',['indexToMatrix',['../namespace_n_puzzle.html#a6d5bee102108921c6f11e4c40808bfad',1,'NPuzzle']]],
   ['isempty',['isEmpty',['../class_priority_queue.html#a3969532d0dc885b0cc69c028637b93f0',1,'PriorityQueue']]],
   ['issolvable',['isSolvable',['../namespace_n_puzzle.html#a4b43a242c57b7f920bbd863fac4fe0fc',1,'NPuzzle']]],
   ['issucceeded',['isSucceeded',['../class_operation_result.html#a539a66bcc4443d6dcf565c5f693f09e0',1,'OperationResult::isSucceeded()'],['../class_search_result.html#a8066d3c995b003243388b6f31db54310',1,'SearchResult::isSucceeded()']]]
--- docs/html/search/functions_5.js
+++ docs/html/search/functions_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['matrixtoindex',['matrixToIndex',['../namespace_n_puzzle.html#a7c3968c3258a41e5dafd22023ed4ea25',1,'NPuzzle']]]
+  ['matrixtoindex',['matrixToIndex',['../namespace_n_puzzle.html#af0fd04a3ffee8e443c0067c15cfcd071',1,'NPuzzle']]]
 ];
