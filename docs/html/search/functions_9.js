a8a89102dd438b4d155edb69a459c0d7e69a03ae
Shindo <cn.shindo@gmail.com>
2016-11-04 02:00:31 -0700

    Update docs

--- docs/html/search/functions_9.js
+++ docs/html/search/functions_9.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['run',['Run',['../class_code_timing.html#afb917b4a7acefd972543ae43551c8cbe',1,'CodeTiming']]]
+  ['run',['Run',['../class_code_timing.html#afb917b4a7acefd972543ae43551c8cbe',1,'CodeTiming']]],
+  ['runvoid',['RunVoid',['../class_code_timing.html#a26f8acb38425de5c73719a34851a7f62',1,'CodeTiming']]]
 ];
